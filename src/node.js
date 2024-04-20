935edc76aa784944d8a6cb8aa705ae5e966cd755
Mike-Benn <mbenn49@wgu.edu>
2024-04-20 04:38:04 -0400

    Add variable,getter,mutator for key

--- src/node.js
+++ src/node.js
@@ -1,12 +1,15 @@
 export { Node };
 
 function Node() {
+    let key = null;
     let value = null;
     let nextNode = null;
 
     const getValue = () => value;
 
     const getNext = () => nextNode;
+
+    const getKey = () => key;
 
     const setValue = (val) => {
         value = val;
@@ -16,10 +19,16 @@ function Node() {
         nextNode = next;
     }
 
+    const setKey = (keyVal) => {
+        key = keyVal;
+    }
+
     return {
         getValue,
         getNext,
+        getKey,
         setValue,
-        setNext
+        setNext,
+        setKey
     }
 }
