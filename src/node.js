9634e23c339bc541a8dd400f909e8b606dca6e6b
Mike-Benn <mbenn49@wgu.edu>
2024-04-21 05:27:39 -0400

    Remove excess whitespace

--- src/node.js
+++ src/node.js
@@ -4,10 +4,13 @@ function Node() {
     let key = null;
     let value = null;
     let nextNode = null;
+    let lastNode = null;
 
     const getValue = () => value;
 
     const getNext = () => nextNode;
+
+    const getLast = () => lastNode;
 
     const getKey = () => key;
 
@@ -19,6 +22,10 @@ function Node() {
         nextNode = next;
     }
 
+    const setLast = (last) => {
+        lastNode = last;
+    }
+
     const setKey = (keyVal) => {
         key = keyVal;
     }
@@ -26,9 +33,11 @@ function Node() {
     return {
         getValue,
         getNext,
+        getLast,
         getKey,
         setValue,
         setNext,
+        setLast,
         setKey
     }
 }
